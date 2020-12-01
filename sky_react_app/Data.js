4a1d39a01386f55c6144479752f19cd06497899c
RyanLee <lzpryan@gmail.com>
2020-12-01 11:51:44 +0000

    Movie data file created

---
+++ sky_react_app/Data.js
@@ -0,0 +1,25 @@
+const movieData = [
+    {
+        title: "Dr No",
+        actor: "Sean Connery",
+        startTime: "09:00",
+        endTime: "11:00",
+        description: "A movie called Dr No starring Sean Connery as James Bond"
+    },
+    {
+        title: "Thunderball",
+        actor: "Sean Connery",
+        startTime: "11:00",
+        endtime: "14:00",
+        description: "A movie called Thunderball starring Sean Connery"
+    },
+    {
+        title: "Moonraker",
+        actor: "Roger Moore",
+        startTime: "15:00",
+        endTime: "17:00",
+        description: "A movie called Moonraker starring Roger Moore"
+    }
+]
+
+export default movieData
